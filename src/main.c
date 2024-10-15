90e757f310a0c069c00562322f8484c9bd210b12
foics <76219388+foics@users.noreply.github.com>
2024-10-14 22:11:12 -0400

    Hello Triangle

--- src/main.c
+++ src/main.c
@@ -9,6 +9,18 @@ const int HEIGHT = 720;
 
 bool should_quit = false;
 
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main() {\n"
+    "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main() {\n"
+    "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\0";
+
 int main(int argc, char *argv[]) {
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
@@ -45,6 +57,90 @@ int main(int argc, char *argv[]) {
 
     glViewport(0, 0, WIDTH, HEIGHT);
 
+    // wireframe rendering
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    float vertices[] = {
+        0.5f, 0.5f, 0.0f,    // top right
+        0.5f, -0.5f, 0.0f,   // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f, 0.5f, 0.0f   // top left
+    };
+
+    unsigned int indices[] = { // note that we start from 0!
+        0, 1, 3,    // first triangle
+        1, 2, 3     // second triangle
+    };
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // xyz
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(0);
+
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    int vertSuccess;
+    char vertInfoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &vertSuccess);
+
+    if (!vertSuccess) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, vertInfoLog);
+        printf("Error shader vertex compilation failed! InfoLog: %s\n", vertInfoLog);
+        return -1;
+    }
+
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);;
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    int fragSuccess;
+    char fragInfoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &fragSuccess);
+
+    if (!fragSuccess) {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, fragInfoLog);
+        printf("Error shader fragment compilation failed! InfoLog: %s\n", fragInfoLog);
+        return -1;
+    }
+
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    int linkSuccess;
+    char linkInfoLog[512];
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &linkSuccess);
+    if (!linkSuccess) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, linkInfoLog);
+        printf("Error shader program linking failed! InfoLog: %s\n", linkInfoLog);
+        return -1;
+    }
+
+    // once shader program is linked the seperate shaders are no longer needed
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
     while (!should_quit) {
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
@@ -57,10 +153,16 @@ int main(int argc, char *argv[]) {
             }
         }
 
+        // render begin
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // swaps buffers aka renders changes
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+        // render end; swaps buffers aka renders changes
         SDL_GL_SwapWindow(window);
     }
 
